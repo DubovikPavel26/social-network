7ad65d7cabe08cf6b94b8f3e9338355b7bbfdccd
pavel <dubovikdmc@gmail.com>
2023-08-13 18:02:52 +0300

    п

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fru.m.wikipedia.org%2Fwiki%2F%25D0%25A4%25D0%25B0%25D0%25B9%25D0%25BB%3ASVG_Logo.svg&psig=AOvVaw1FPYP4rb3kgFWxc-iWYR26&ust=1692014235254000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICkt63K2YADFQAAAAAdAAAAABAE" alt="logo"/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img
+                    src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg'
+                    alt='content'/>
+            </div>
+
+
+            <div>
+                ava + descr
+            </div>
+
+            <div>
+                My post
+                <div>New post</div>
+                <div>
+                    <div>post 12</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
